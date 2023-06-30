dd14ec3a5adff91949c7e6c953bf9187979ec175
Alex-VSH <aisb@tut.by>
2023-06-30 22:23:14 +0300

    repair tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,8 +2,9 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1) == 2
+    assert arrs.get('test', 0) == "t"
+    assert arrs.get('test', -2) is None
 
 
 def test_slice():
